8fab25f3d4f95b2e6fa440662d2d27443719e31c
Jean-Francois Dockes <jf@dockes.org>
2024-10-13 10:17:22 +0200

    bumped version

--- src/common/autoconfig-mac.h
+++ src/common/autoconfig-mac.h
@@ -40,7 +40,7 @@
 #define ICONV_CONST 
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.40.2"
+#define PACKAGE_VERSION "1.40.3"
 
 /* Use QTextBrowser to implement the preview windows */
 #undef PREVIEW_FORCETEXTBROWSER
--- src/common/autoconfig-win.h
+++ src/common/autoconfig-win.h
@@ -44,7 +44,7 @@
 #define IDX_THREADS 1
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.40.2"
+#define PACKAGE_VERSION "1.40.3"
 
 /* Use QTextBrowser to implement the preview windows */
 #undef PREVIEW_FORCETEXTBROWSER
